28de09a557b7012a5e0d971888f417525ffcc2f2
Kenneth Lewenhagen <lewenhagen@live.com>
2017-10-17 10:29:09 +0200

    added configfile and awesome fixes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ Generates the heatmap and render the image in template
 """
 
 import functions
+import config
 from flask import Flask, render_template, request
 import os, glob
 
@@ -13,8 +14,7 @@ import os, glob
 
 app = Flask(__name__)
 
-y_ticks = ["hours", "months"]
-x_ticks = ["weekdays", "months"]
+
 
 
 
@@ -26,7 +26,7 @@ def main():
     """
 
 
-    return render_template("index.html", xticks=x_ticks, yticks=y_ticks)
+    return render_template("index.html", xticks=config.x_ticks, yticks=config.y_ticks)
 
 
 @app.route("/hotspot", methods=["POST"])
@@ -34,44 +34,47 @@ def hotspot():
     """ Hotspot route """
 
     if request.method == "POST":
-        # print(request.form)
+        filename = request.form["setupFilename"] + ".png"
 
-        hotspot_one = {
-            "filename": request.form["setupFilename"] + ".png",
-            "title": request.form["setupTitle"],
-            "xticks": functions.get_ticks(request.form["setupXticks"]),
-            "yticks": functions.get_ticks(request.form["setupYticks"]),
-            "labels": {
-                "xlabel": request.form["setupXticks"],
-                "ylabel": request.form["setupYticks"]
+        if filename in os.listdir("static"):
+            return render_template("index.html", xticks=config.x_ticks, yticks=config.y_ticks, duplicate=filename)
+        else:
+            hotspot_one = {
+                "filename": filename,
+                "title": request.form["setupTitle"],
+                "xticks": functions.get_ticks(request.form["setupXticks"]),
+                "yticks": functions.get_ticks(request.form["setupYticks"]),
+                "labels": {
+                    "xlabel": request.form["setupXticks"],
+                    "ylabel": request.form["setupYticks"]
+                }
             }
-        }
 
-        print(hotspot_one["filename"])
-        # hotspot_two = {
-        #     "filename": "map2.png",
-        #     "title": "Temporal hotspot 2",
-        #     "xticks": functions.get_ticks("weekdays"),
-        #     "yticks": functions.get_ticks("hours"),
-        #     "labels": {
-        #         "xlabel": "Weekdays",
-        #         "ylabel": "Hours"
-        #     }
-        # }
 
-        # Get a 2d list, dataframe
-        hotspot_one["data"] = functions.get_data(hotspot_one)
-        # hotspot_two["data"] = functions.get_data(hotspot_two)
+            # hotspot_two = {
+            #     "filename": "map2.png",
+            #     "title": "Temporal hotspot 2",
+            #     "xticks": functions.get_ticks("weekdays"),
+            #     "yticks": functions.get_ticks("hours"),
+            #     "labels": {
+            #         "xlabel": "Weekdays",
+            #         "ylabel": "Hours"
+            #     }
+            # }
 
-        # Creates the hotspot
-        functions.create_hotspot(hotspot_one)
-        # functions.create_hotspot(hotspot_two)
+            # Get a 2d list, dataframe
+            hotspot_one["data"] = functions.get_data(hotspot_one)
+            # hotspot_two["data"] = functions.get_data(hotspot_two)
 
-        # The filenames for hotspot images
-        filenames = []
+            # Creates the hotspot
+            functions.create_hotspot(hotspot_one)
+            # functions.create_hotspot(hotspot_two)
 
-        filenames.append(hotspot_one["filename"])
-        # filenames.append(hotspot_two["filename"])
+            # The filenames for hotspot images
+            filenames = []
+
+            filenames.append(hotspot_one["filename"])
+            # filenames.append(hotspot_two["filename"])
 
     return render_template("hotspot.html", hotspots=filenames)
 
@@ -80,6 +83,7 @@ def created():
     """
     Created route
     """
+
     created_hotspots = []
     view_hotspots = []
 
---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""
+Configuration file for hotspot
+"""
+y_ticks = ["hours", "months"]
+x_ticks = ["weekdays", "months"]
