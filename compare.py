19a7fd3b7e0216d98b3586f77cb86bc8d6c0cdf7
Kenneth Lewenhagen <lewenhagen@live.com>
2018-02-11 15:04:46 +0100

    some more tests on comparing

--- compare.py
+++ compare.py
@@ -8,6 +8,11 @@ import pandas as pd
 from scipy.stats import norm
 from numpy.linalg import eig
 import numpy as np
+# get percent
+# from difflib import SequenceMatcher
+#
+# def similar(a, b):
+#     return SequenceMatcher(None, a, b).ratio()
 
 def print_data(data):
     """
@@ -19,9 +24,24 @@ def compare(file_a, file_b):
     """
     Compare two hotspots
     """
+
+
+    # print(np.allclose(file_a, file_b))
     # a = np.matrix(file_a)
     # b = np.matrix(file_b)
     # x = norm(eig(file_a) - eig(file_b))
-    y = norm.pdf(file_a - file_b)
+    # y = norm.pdf(file_a - file_b)
     # file_a.drop(file_a.head(1).index, inplace=True)
-    print(y)
+
+
+    # get -1, 0 and 1
+    # y = np.sign(file_a - file_b)
+    # print(y)
+
+    # get nr of elements are the same
+    # print((file_a == file_b).sum())
+    # ELLER
+    # print(np.count_nonzero(file_a == file_b))
+
+    # does not work for matrices (yet)
+    # print(similar(file_a, file_b))
