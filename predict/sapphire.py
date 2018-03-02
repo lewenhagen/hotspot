1c417f0c8aec4a65e8bc91586fcacce54f14e462
Andreas Arnesson <Andreas_osby@hotmail.com>
2018-03-02 15:30:17 +0100

    first version for parsing 2011-2014

--- predict/sapphire.py
+++ predict/sapphire.py
@@ -5,8 +5,15 @@ Main file for predicting hotspots
 import json
 import numpy
 import sys
+import itertools
+import pandas as pd
+import matplotlib.pylab as plt
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 15, 6 # donno
 sys.path.append('..')
 from gi.getis import Gi
+from aoristic import aoristic
+from aoristic import parse as parser
 
 
 class Sapphire:
@@ -45,7 +52,22 @@ class Sapphire:
         for i, row in enumerate(matrix):
             print(i, row)
 
+    def setup(self):
+        units = json.load(open("../units.json", "r"))
+        unit_y = units["hours"]
+        unit_x = units["days"]
+        all_data = list()
+        for y in range(1, 5):
+            for m in range(1, 13):
+                search = "201{}-{}".format(y, m if m > 9 else "0" + str(m))
+                raw_data = parser.csv_to_dict(search, "datestart", "total_v2.csv")
+                aoristic_data = [[0 for x in range(unit_x["size"])] for y in range(unit_y["size"])] # x y
+                aoristic.aoristic_method(raw_data, aoristic_data, unit_x, unit_y)
+                arr = numpy.matrix(aoristic_data).flat
+                all_data = list(itertools.chain(all_data, arr))
 
+        plt.plot(all_data) # plot timeseries
+        plt.show()
 
 if __name__ == "__main__":
     all = json.load(open("test/all.json"))
@@ -54,10 +76,11 @@ if __name__ == "__main__":
         dic[i] = numpy.matrix(l[1])
     res = dic[len(dic)-1]
     del dic[len(dic)-1]
-
+    #
     s = Sapphire(dic, res)
-    s.add()
-    s.calc_gi()
+    s.setup()
+    # s.add()
+    # s.calc_gi()
 
 # 1 skapa en hotspot av alla tidigare hur skiljer den hotspot från framtid?
 # 2 kan man kolla skillnad mellan matriser och försöka bygga den nya från den innan med hjälpa av average skillnader?
