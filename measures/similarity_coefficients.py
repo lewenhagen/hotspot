13c5905003f4c9103e4f2f7ee0ce337b7d188552
lewenhagen <lewenhagen@live.com>
2018-03-26 15:58:35 +0200

    changed some

--- measures/similarity_coefficients.py
+++ measures/similarity_coefficients.py
@@ -1,6 +1,7 @@
 #/usr/bin/env python3
 """
 Main file for different similarity coefficients
+setup_data makes it possible to use -1 as well, adds to TT/TF/FT counter.
 """
 
 import numpy as np
@@ -18,21 +19,30 @@ def setup_data(left, right):
 
     if len(left) == len(right):
         for index, val in enumerate(left):
-            if left[index] == 1 and right[index] == 0: # true false
+            if left[index] in (1, -1) and right[index] == 0: # true_false
                 true_false += 1
-            elif left[index] == 0 and right[index] == 1: # false true
-                false_true += 1
-            elif left[index] == 0 and right[index] == 0: # false false
-                false_false += 1
-            elif left[index] == 1 and right[index] == 1: # true true
+            elif left[index] == 1 and right[index] == 1: # true_true
                 true_true += 1
+            elif left[index] == -1 and right[index] == -1:
+                true_true += 1
+            elif left[index] == 0 and right[index] in (1, -1): # false_true
+                false_true += 1
 
-            elif left[index] == -1 and right[index] == 0: # true false
-                true_false += 1
-            elif left[index] == 0 and right[index] == -1: # false true
-                false_true += 1
-            elif left[index] == -1 and right[index] == -1: # true true
-                true_true += 1
+            # if left[index] == 1 and right[index] == 0: # true false
+            #     true_false += 1
+            # elif left[index] == 0 and right[index] == 1: # false true
+            #     false_true += 1
+            # elif left[index] == 0 and right[index] == 0: # false false
+            #     false_false += 1
+            # elif left[index] == 1 and right[index] == 1: # true true
+            #     true_true += 1
+            #
+            # elif left[index] == -1 and right[index] == 0: # true false
+            #     true_false += 1
+            # elif left[index] == 0 and right[index] == -1: # false true
+            #     false_true += 1
+            # elif left[index] == -1 and right[index] == -1: # true true
+            #     true_true += 1
 
     return (true_true, true_false, false_true)
 
@@ -87,18 +97,27 @@ if __name__ == "__main__":
     partial_left =  [0, 0, 1, 0, 0, 1, 0, 0, 1, 0] # 3 / 5 = 6 / 10 = 60 / 100
     partial_right = [1, 0, 1, 0, 0, 1, 1, 0, 1, 0]
 
+    all_values_left =  [-1, -1, 1, 0, 2, 1, 0, 0, 1, 0]
+    all_values_right = [0, -1, 1, 0, 0, 1, 0, 0, 1, 0]
+
     print("Jaccard unique:", jaccard(unique_left, unique_right))
     print("Jaccard identical:", jaccard(identical_left, identical_right))
     print("Jaccard partial:", jaccard(partial_left, partial_right))
+    print("Jaccard all values:", jaccard(all_values_left, all_values_right))
+
 
     print("Sorensen-Dice unique:", s_dice(unique_left, unique_right))
     print("Sorensen-Dice identical:", s_dice(identical_left, identical_right))
     print("Sorensen-Dice partial:", s_dice(partial_left, partial_right))
+    print("Sorensen-Dice all values:", s_dice(all_values_left, all_values_right))
+
 
     print("Kulczynski unique:", kulczynski(unique_left, unique_right))
     print("Kulczynski identical:", kulczynski(identical_left, identical_right))
     print("Kulczynski partial:", kulczynski(partial_left, partial_right))
+    print("Kulczynski all values:", kulczynski(all_values_left, all_values_right))
 
     print("Ochai unique:", ochai(unique_left, unique_right))
     print("Ochai identical:", ochai(identical_left, identical_right))
     print("Ochai partial:", ochai(partial_left, partial_right))
+    print("Ochai all values:", ochai(all_values_left, all_values_right))
